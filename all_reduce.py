7023247d50acb38294d4873d071e40cc3db2dc20
wangguoteng.p <wangguoteng@sensetime.com>
2023-05-16 14:47:59 +0800

    add node view

--- all_reduce.py
+++ all_reduce.py
@@ -1,22 +1,22 @@
-'''Copyright The Microsoft DeepSpeed Team'''
+"""Copyright The Microsoft DeepSpeed Team"""
 
 import torch
 import sys, os, time
-
 
 
 from utils import *
 from constants import *
 from accelerator import accelerator
 
+
 def get_accelerator():
     return accelerator
 
 
 def timed_all_reduce(input, args):
-    if args.dist == 'torch':
+    if args.dist == "torch":
         import torch.distributed as dist
-    elif args.dist == 'deepspeed':
+    elif args.dist == "deepspeed":
         import deepspeed.comm as dist
 
     sync_all()
@@ -36,9 +36,9 @@ def timed_all_reduce(input, args):
     avg_duration = duration / args.trials
     size = input.element_size() * input.nelement()
     n = dist.get_world_size()
-    tput, busbw = get_bw('all_reduce', size, avg_duration, args)
+    tput, busbw = get_bw("all_reduce", size, avg_duration, args)
     tput_str, busbw_str, duration_str = get_metric_strings(args, tput, busbw, avg_duration)
-    desc = f'{input.nelement()}x{input.element_size()}'
+    desc = f"{input.nelement()}x{input.element_size()}"
 
     if not args.raw:
         size = convert_size(size)
@@ -47,13 +47,13 @@ def timed_all_reduce(input, args):
 
 
 def run_all_reduce(local_rank, args):
-    if args.dist == 'torch':
+    if args.dist == "torch":
         import torch.distributed as dist
-    elif args.dist == 'deepspeed':
+    elif args.dist == "deepspeed":
         import deepspeed.comm as dist
 
     # Prepare benchmark header
-    print_header(args, 'all_reduce')
+    print_header(args, "all_reduce")
 
     world_size = dist.get_world_size()
     global_rank = dist.get_rank()
@@ -68,14 +68,15 @@ def run_all_reduce(local_rank, args):
         for M in M_LIST:
             global_rank = dist.get_rank()
             try:
-                mat = torch.ones(world_size, M,
-                                 dtype=getattr(torch, args.dtype)).to(get_accelerator().device_name(local_rank))
+                mat = torch.ones(world_size, M, dtype=getattr(torch, args.dtype)).to(
+                    get_accelerator().device_name(local_rank)
+                )
                 sync_all()
-                input = ((mat.mul_(float(global_rank))).view(-1))
+                input = (mat.mul_(float(global_rank))).view(-1)
             except RuntimeError as e:
-                if 'out of memory' in str(e):
+                if "out of memory" in str(e):
                     if dist.get_rank() == 0:
-                        print('WARNING: Ran out of GPU memory. Exiting comm op.')
+                        print("WARNING: Ran out of GPU memory. Exiting comm op.")
                     sync_all()
                     break
                 else:
@@ -85,25 +86,105 @@ def run_all_reduce(local_rank, args):
     else:
         # Send the biggest message size our GPUs can fit. If you're facing OOM errors, reduce the mem_factor
         # Don't need output tensor, so we double mem_factor
-        elements_per_gpu = max_numel(comm_op='all_reduce',
-                                     dtype=getattr(torch, args.dtype),
-                                     mem_factor=args.mem_factor * 2,
-                                     local_rank=local_rank,
-                                     args=args)
+        elements_per_gpu = max_numel(
+            comm_op="all_reduce",
+            dtype=getattr(torch, args.dtype),
+            mem_factor=args.mem_factor * 2,
+            local_rank=local_rank,
+            args=args,
+        )
         try:
-            mat = torch.ones(elements_per_gpu, dtype=getattr(torch,
-                                                             args.dtype)).to(get_accelerator().device_name(local_rank))
-            input = ((mat.mul_(float(global_rank))).view(-1))
+            mat = torch.ones(elements_per_gpu, dtype=getattr(torch, args.dtype)).to(
+                get_accelerator().device_name(local_rank)
+            )
+            input = (mat.mul_(float(global_rank))).view(-1)
         except RuntimeError as e:
-            if 'out of memory' in str(e):
+            if "out of memory" in str(e):
                 if dist.get_rank() == 0:
-                    print('WARNING: Ran out of GPU memory. Try to reduce the --mem-factor argument!')
+                    print("WARNING: Ran out of GPU memory. Try to reduce the --mem-factor argument!")
                 sync_all()
                 return
             else:
                 raise e
         sync_all()
         timed_all_reduce(input, args)
+
+
+def timed_all_reduce_node_view(input, args):
+    if args.dist == "torch":
+        import torch.distributed as dist
+    elif args.dist == "deepspeed":
+        import deepspeed.comm as dist
+
+    sync_all()
+    # Warmups, establish connections, etc.
+    for i in range(args.warmups):
+        dist.all_reduce(input, async_op=args.async_op)
+    sync_all()
+
+    # time the actual comm op trials times and average it
+    pre = time.perf_counter()
+    for i in range(args.trials):
+        dist.all_reduce(input, async_op=args.async_op)
+    sync_cuda()
+    rank_wait_time = time.perf_counter() - pre
+
+    sync_all()
+    duration = time.perf_counter() - pre
+
+    # maintain and clean performance data
+    avg_duration = duration / args.trials
+    size = input.element_size() * input.nelement()
+    n = dist.get_world_size()
+    tput, busbw = get_bw("all_reduce", size, avg_duration, args)
+    tput_str, busbw_str, duration_str = get_metric_strings(args, tput, busbw, avg_duration)
+    desc = f"{input.nelement()}x{input.element_size()}"
+
+    if not args.raw:
+        size = convert_size(size)
+
+    print_node_0(f"{size:<20} {desc:25s} {duration_str:20s} {rank_wait_time:20s} {tput_str:20s} {busbw_str:20s}")
+
+
+def run_all_reduce_node_view(local_rank, args):
+    if args.dist == "torch":
+        import torch.distributed as dist
+    elif args.dist == "deepspeed":
+        import deepspeed.comm as dist
+
+    # Prepare benchmark header
+    print_header_node_view(args, "all_reduce")
+
+    world_size = dist.get_world_size()
+    global_rank = dist.get_rank()
+
+    if args.scan:
+        M_LIST = []
+        for x in (2**p for p in range(1, args.maxsize)):
+            M_LIST.append(x)
+
+        sync_all()
+        # loop over various tensor sizes
+        for M in M_LIST:
+            global_rank = dist.get_rank()
+            try:
+                mat = torch.ones(world_size, M, dtype=getattr(torch, args.dtype)).to(
+                    get_accelerator().device_name(local_rank)
+                )
+                sync_all()
+                input = (mat.mul_(float(global_rank))).view(-1)
+            except RuntimeError as error:
+                if "out of memory" in str(error):
+                    if dist.get_rank() == 0:
+                        print("WARNING: Ran out of GPU memory. Exiting comm op.")
+                    sync_all()
+                    break
+                else:
+                    raise error
+            sync_all()
+            timed_all_reduce_node_view(input, args)
+    else:
+        print("Not support ")
 
 
 if __name__ == "__main__":
